83210bf5068a92e5a598fae3f25f9e34944e0c71
nikitamoiseev <nikita-moiseev-2003@yandex.ru>
2024-10-21 12:54:58 +0300

    add PartName ServiceType enums

--- src/main/java/ru/unn/autorepairshop/domain/enums/PartName.java
+++ src/main/java/ru/unn/autorepairshop/domain/enums/PartName.java
@@ -2,7 +2,25 @@ package ru.unn.autorepairshop.domain.enums;
 
 public enum PartName {
 
-    //todo дописать
-    PLACEHOLDER
+    ENGINE_OIL,               // Моторное масло
+    OIL_FILTER,               // Масляный фильтр
+    AIR_FILTER,               // Воздушный фильтр
+    FUEL_FILTER,              // Топливный фильтр
+    CABIN_FILTER,             // Салонный фильтр
+    SPARK_PLUG,               // Свечи зажигания
+    BRAKE_PADS,               // Тормозные колодки
+    BRAKE_DISCS,              // Тормозные диски
+    WINDSHIELD_WIPERS,        // Дворники (щетки стеклоочистителя)
+    COOLANT,                  // Охлаждающая жидкость (антифриз)
+    TRANSMISSION_FLUID,       // Трансмиссионное масло
+    BRAKE_FLUID,              // Тормозная жидкость
+    POWER_STEERING_FLUID,     // Жидкость для гидроусилителя руля
+    BATTERY,                  // Аккумулятор
+    TIRES,                    // Шины
+    BULBS,                    // Лампы освещения
+    BELTS,                    // Ремни (приводные, ГРМ)
+    CLUTCH_KIT,               // Комплект сцепления
+    FUSES,                    // Предохранители
+    PLACEHOLDER               // Заглушка для новых расходников
 
 }
